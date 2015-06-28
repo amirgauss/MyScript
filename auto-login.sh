6332a81d5021fafed769f4279941872f502b06c4
amirgauss <amiriamiri@chmail.ir>
2015-06-28 14:24:38 +0430

    first commit

---
+++ auto-login.sh
@@ -0,0 +1,1 @@
+curl -s https://192.168.123.2:4081/login/index.php -d kerio_username=guest&kerio_password=123654&login-button=Login
---
+++ auto-login2.sh
@@ -0,0 +1,1 @@
+wget --save-cookies cookies.txt --keep-session-cookies --no-check-certificate --post-data="kerio_username=guest&kerio_password=123654" "https://192.168.123.2:4081/login/index.php"
